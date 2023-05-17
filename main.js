c6c537a058e42a634641c1f718fb1e8d9d74e0c7
Taanish08 <123229789+Taanish08@users.noreply.github.com>
2023-05-17 16:55:39 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+Canvas=document.getElementById("canvos");
+ctx=canvas.getContext("2d")
+
+
+ctx.beginPath();
+ctx.strokeStyle ="red";
+ctx.lineWidth = 1;
+ctx.rect(150,143, 430, 200);
+ctx.strokeStyle();
+
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth= 1;
+ctx.arc(250 , 210 , 40, 0 , 2 * Math.PI);
+ctx.strokeStyle();
+
+ctx.beginPath();
+ctx.strokeStyle= "black";
+ctx.lineWidth= 1;
+ctx.arc(270 , 210 , 40, 0 , 2 * Math.PI);
+ctx.strokeStyle();
+
+
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth= 1;
+ctx.arc(290 , 210 , 40, 0 , 2 * Math.PI);
+ctx.strokeStyle();
